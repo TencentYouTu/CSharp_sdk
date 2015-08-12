24b260536a03c4e5f6d43848dd45b998a2e76796
TencentYouTu <youtu@tencent.com>
2015-08-12 19:07:02 +0800

    fix
    fix

--- Sample/Program.cs
+++ Sample/Program.cs
@@ -11,15 +11,11 @@ namespace Sample
         static void Main(string[] args)
         {
             // 设置为你自己的密钥对
-            //string appid = "you appid";
-            //string secretId = "you secretid";
-            //string secretKey = "you secretkey";
-            //string userid = "请用qq号代替";
+            string appid = "you appid";
+            string secretId = "you secretid";
+            string secretKey = "you secretkey";
+            string userid = "请用qq号代替";
 
-            string appid = "1000031";
-            string secretId = "AKIDUIDlPDt5mZutfr46NT0GisFcQh1nMOox";
-            string secretKey = "ind5yAd55ZspBc7MCANcxEjuXi8YU8RL";
-            string userid = "380549494";
 
 
             Conf.Instance().setAppInfo(appid, secretId, secretKey, userid);
